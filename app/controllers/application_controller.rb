ff6ef2c2f724dbe9e1d95e766064020b6d6c23c4
Mr.Geek <luckyhappening777@gmail.com>
2018-06-07 22:07:06 +0900

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:index]
+  def configure_permitted_parameters
+    added_attrs = [:name, :email]
+    devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+  end
+  def after_sign_in_path_for(resource)
+    chat_rooms_path(resource)
+  end
+  def after_sign_out_path_for(resource)
+    chat_rooms_path(resource)
+  end
 end
