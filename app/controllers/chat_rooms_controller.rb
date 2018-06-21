1f99a547535ad40c989213eeea21cfc082393003
Kobayashi Ikuo <kobayashi@signalysis.co.jp>
2018-06-21 13:58:18 +0900

    added relationship with participants

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatRoomsController < ApplicationController
   before_action :set_chat_room, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /chat_rooms
   # GET /chat_rooms.json
