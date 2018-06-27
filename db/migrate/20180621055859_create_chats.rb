40fa7da9b6b37b710b9c4b9b58f7be2eaff7d712
tomdemort <tomofumi.sato.2y@stu.hosei.ac.jp>
2018-06-28 00:55:47 +0900

    Add Chat Table

--- db/migrate/20180621055859_create_chats.rb
+++ db/migrate/20180621055859_create_chats.rb
@@ -1,9 +1,9 @@
 class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
-      t.references :chat_room_id
+      t.integer :chat_room_id
       t.string :message
-      t.references :user_id
+      t.integer :user_id
 
       t.timestamps
     end
