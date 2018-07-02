dd2844df45bda165c5d4f877a7435dd6d77fee6c
takahiro <takahiro.678.yo@gmail.com>
2018-07-02 22:36:43 +0900

    create chatroom list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_100254) do
+ActiveRecord::Schema.define(version: 2018_07_01_033141) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_13_100254) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "removed"
   end
 
   create_table "participants", force: :cascade do |t|
