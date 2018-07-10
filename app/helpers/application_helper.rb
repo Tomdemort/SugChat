abccc736189c0128554228aead10387310e0e29a
hk-nyao <shota.ikawa.6e@stu.hosei.ac.jp>
2018-07-11 08:12:11 +0900

    modified title logo and chats page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def simple_time(time)
-    time.strftime("%Y-%m-%d　%H:%M　")
-  end 
+    #time.strftime("%Y-%m-%d　%H:%M　")
+    str= "[" + time.strftime("%H:%M") + "]"
+    return str
+  end
+
+  def simple_time2(time)
+    time.strftime("%Y-%m-%d")
+  end
 end
